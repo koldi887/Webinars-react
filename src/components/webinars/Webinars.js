a89eeddb5c657508b53a1abc127992e99e727fd8
Koldi <74019592+koldi887@users.noreply.github.com>
2021-11-11 13:30:50 -0600

    fix

--- src/components/webinars/Webinars.js
+++ src/components/webinars/Webinars.js
@@ -1,54 +1,56 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import classes from "./webinars.module.css";
-import {WebinarsData} from "./WebinarsData";
+import { WebinarsData } from "./WebinarsData";
 import AddWebinarContainer from "./add-webinars/AddWebinarContainer";
 
 export default function Webinars() {
-    const c = classes
-    const [currentState, setCurrentState] = useState(WebinarsData)
-    const [hideAddWebinar, setHideAddWebinar] = useState(false)
+  const c = classes;
+  const [currentState, setCurrentState] = useState(WebinarsData);
+  const [hideAddWebinar, setHideAddWebinar] = useState(false);
 
-    return (
+  return (
+    <div className={c.webinarWrapper}>
+      <div className={c.addButtonBlock}>
+        <h3>Add new webinar:</h3>
+        <i
+          className={`fas fa-plus fa-2x ${c.faPlus}`}
+          onClick={() => setHideAddWebinar(true)}
+        />
+      </div>
 
-        <div className={c.webinarWrapper}>
+      {hideAddWebinar && (
+        <AddWebinarContainer
+          currentState={currentState}
+          setCurrentState={setCurrentState}
+          setHide={setHideAddWebinar}
+        />
+      )}
 
-            <div className={c.addButtonBlock}>
-                <h3>Add new webinar:</h3>
-                <i className={`fas fa-plus fa-2x ${c.faPlus}`}
-                   onClick={() => setHideAddWebinar(true)}
-                />
+      {currentState.map((item, index) => (
+        <div className={c.webinarBlock} key={index}>
+          <img src={item.image} alt="webinar" />
+          <div className={c.webinarInfo}>
+            <div className={c.subtitle}>
+              <strong>Webinar</strong>
             </div>
 
-            {hideAddWebinar && <AddWebinarContainer
-                currentState={currentState}
-                setCurrentState={setCurrentState}
-                setHide={setHideAddWebinar}
-            />}
+            <div className={c.title}>
+              <strong>{item.title}</strong>
+            </div>
 
-            {currentState.map((item, index) => (
-                <div className={c.webinarBlock} key={index}>
-                    <img src={item.image} alt="webinar"/>
-                    <div className={c.webinarInfo}>
-
-                        <div className={c.subtitle}>
-                            <strong>Webinar</strong>
-                        </div>
-
-                        <div className={c.title}>
-                            <strong>{item.title}</strong>
-                        </div>
-
-                        <div className={c.topicsList}>
-                            <ul className={'list'}>
-                                {item.subtopic.map((item, index) => <li key={index}>{item}</li>)}
-                            </ul>
-                        </div>
-                        <div className={c.btn}>
-                            <button className={`buttons`}>Read more</button>
-                        </div>
-                    </div>
-                </div>
-            ))}
+            <div className={c.topicsList}>
+              <ul className={"list"}>
+                {item.subtopic.map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
+              </ul>
+            </div>
+            <div className={c.btn}>
+              <button className={`buttons`}>Read more</button>
+            </div>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
